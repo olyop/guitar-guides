741bff3507415c03ed7c29a85f23e997d3b8ffa5
Oliver <oly.rbsc@gmail.com>
2017-12-07 18:52:51 +1100

    Update

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -2,47 +2,64 @@ import React from 'react'
 
 import { NavLink } from 'react-router-dom'
 
+import GuitarIcon from './guitar-icon.png'
+import BassIcon from './bass-icon.png'
+
 import './menu.css'
 
 const Menu = props => (
 	<div id="menu">
 		<div className="menu-inner">
-		
-			<h1><span>Menu</span></h1>
 
 			<div className="menu-split">
         
         <div className="menu-split-item menu-guitar">
-          <h2>Guitar</h2>
+          <div className="menu-split-heading">
+						<h2><span>Guitar</span></h2>
+					</div>
           <div className="menu-links">
-            <NavLink
-              onClick={ () => props.handleHamburger() }
-              to="/guitar/chords">Chords</NavLink>
-
-            <NavLink
-              onClick={ () => props.handleHamburger() }
-              to="/guitar/scales">Scales</NavLink>
-
-            <NavLink
-              onClick={ () => props.handleHamburger() }
-              to="/guitar/Songs">Songs</NavLink>
+						<ul>
+							<li>
+								<NavLink
+              		onClick={ () => props.handleHamburger() }
+              		to="/guitar/chords">Chords</NavLink>
+							</li>
+							<li>
+								<NavLink
+									onClick={ () => props.handleHamburger() }
+									to="/guitar/scales">Scales</NavLink>
+							</li>
+							<li>
+								<NavLink
+									onClick={ () => props.handleHamburger() }
+									to="/guitar/Songs">Songs</NavLink>
+							</li>
+						</ul>
           </div>
         </div>
 
         <div className="menu-split-item menu-bass">
-          <h2>Bass</h2>
+          <div className="menu-split-heading">
+						<h2><span>Bass</span></h2>
+					</div>
           <div className="menu-links">
-            <NavLink
-              onClick={ () => props.handleHamburger() }
-              to="/bass/chords">Chords</NavLink>
-
-            <NavLink
-              onClick={ () => props.handleHamburger() }
-              to="/bass/scales">Scales</NavLink>
-
-            <NavLink
-              onClick={ () => props.handleHamburger() }
-              to="/bass/Songs">Songs</NavLink>
+						<ul>
+							<li>
+								<NavLink
+              		onClick={ () => props.handleHamburger() }
+              		to="/bass/chords">Chords</NavLink>
+							</li>
+							<li>
+								<NavLink
+									onClick={ () => props.handleHamburger() }
+									to="/bass/scales">Scales</NavLink>
+							</li>
+							<li>
+								<NavLink
+									onClick={ () => props.handleHamburger() }
+									to="/bass/Songs">Songs</NavLink>
+							</li>
+						</ul>
           </div>
         </div>
         
