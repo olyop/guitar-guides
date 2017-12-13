d6e5515592a04b9a8a24fb1b4e3a3517327a72fc
Oliver <oly.rbsc@gmail.com>
2017-12-13 23:14:59 +1100

    Update

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -10,26 +10,73 @@ const Home = props => (
 	<div id="home">
 
 		<div className="home-section home-left">
-			<div className="home-section-inner"
-				style={{ backgroundImage: 'url('+ guitarStock + ')' }}>
-					<h2>Learn</h2>
-					<h1>Guitar</h1>
+			<div className="home-section-inner">
+        
+        <div className="home-section-image"
+          style={{ backgroundImage: 'url('+ guitarStock + ')' }} />
+        <h2>Learn</h2>
+        
+        <h1>Guitar</h1>
+        
+        <div className="home-section-line" />
+        
+        <div className="home-section-content">
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+          </ul>
+        </div>
+        
 			</div>
 		</div>
 		
 		<div className="home-section home-middle">
-			<div className="home-section-inner"
-				style={{ backgroundImage: 'url('+ bassStock + ')' }}>
-					<h2>Learn</h2>
-					<h1>Bass</h1>
+			<div className="home-section-inner">
+        
+        <div className="home-section-image"
+          style={{ backgroundImage: 'url('+ bassStock + ')' }} />
+        
+        <h2>Learn</h2>
+        
+        <h1>Bass</h1>
+        
+        <div className="home-section-line" />
+        
+        <div className="home-section-content">
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+          </ul>
+        </div>
+        
 			</div>
 		</div>
 		
 		<div className="home-section home-right">
-			<div className="home-section-inner"
-				style={{ backgroundImage: 'url('+ musicStock + ')' }}>
-					<h2>Learn</h2>
-					<h1>Music</h1>
+			<div className="home-section-inner">
+        
+        <div className="home-section-image"
+          style={{ backgroundImage: 'url('+ musicStock + ')' }} />
+        
+        <h2>Learn</h2>
+        
+        <h1>Theory</h1>
+        
+        <div className="home-section-line" />
+        
+        <div className="home-section-content">
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+          </ul>
+        </div>
+        
 			</div>
 		</div>
 
