028145f5295e25153e1897c9662819d158090804
Oliver <oly.rbsc@gmail.com>
2018-02-14 17:34:19 +1100

    Update

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import Home from './pages/home/home'
 import Guitar from './pages/guitar/guitar'
 import Bass from './pages/bass/bass'
 import Theory from './pages/theory/theory'
+import AccountHover from './account-hover/account-hover'
 
 // Import Components
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
@@ -91,6 +92,8 @@ class Index extends React.Component {
 								globalText={globalText}
 								logIn={this.logIn} />
             )}
+						
+						<AccountHover />
 
             {isAccountLoggedIn ? (
               <div id="content">
