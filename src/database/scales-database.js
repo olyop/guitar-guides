decb422b5abf4e21ed032b3c55c1394083f8d65a
Oliver <oly.rbsc@gmail.com>
2018-02-19 14:56:39 +1100

    Update

--- src/database/scales-database.js
+++ src/database/scales-database.js
@@ -6,82 +6,82 @@ const scalesDatabase = [
 			{
 				note: 'E',
 				fret: 0,
-				string: 1
+				string: 6
 			},
 			{
 				note: 'F#',
 				fret: 2,
-				string: 1
+				string: 6
 			},
 			{
 				note: 'G',
 				fret: 3,
-				string: 1
+				string: 6
 			},
 			{
 				note: 'A',
 				fret: 0,
-				string: 2
+				string: 5
 			},
 			{
 				note: 'B',
 				fret: 2,
-				string: 2
+				string: 5
 			},
 			{
 				note: 'C',
 				fret: 3,
-				string: 2
+				string: 5
 			},
 			{
 				note: 'D',
 				fret: 0,
-				string: 3
+				string: 4
 			},
 			{
 				note: 'E',
 				fret: 2,
-				string: 3
+				string: 4
 			},
 			{
 				note: 'G',
 				fret: 0,
-				string: 4
+				string: 3
 			},
 			{
 				note: 'A',
 				fret: 2,
-				string: 4
+				string: 3
 			},
 			{
 				note: 'B',
 				fret: 0,
-				string: 5
+				string: 2
 			},
 			{
 				note: 'C',
 				fret: 1,
-				string: 5
+				string: 2
 			},
 			{
 				note: 'D',
 				fret: 3,
-				string: 5
+				string: 2
 			},
 			{
 				note: 'E',
 				fret: 0,
-				string: 6
+				string: 1
 			},
 			{
 				note: 'F#',
 				fret: 2,
-				string: 6
+				string: 1
 			},
 			{
 				note: 'G',
 				fret: 3,
-				string: 6
+				string: 1
 			}
 		]
 	}
