754f6470db5f4c7d1b57ebae3ba5e08c52e265b5
Oliver <oly.rbsc@gmail.com>
2018-02-21 15:50:35 +1100

    Update

--- src/database/scales-database.js
+++ src/database/scales-database.js
@@ -7,92 +7,95 @@ const scalesDatabase = [
 	[], // D
 	[], // D#
 	[
-		{
-			id: 'tgVmVCpXMB',
-			name: 'E minor',
-			tab: [
-				{
-					note: 'E',
-					fret: 0,
-					string: 6
-				},
-				{
-					note: 'F#',
-					fret: 2,
-					string: 6
-				},
-				{
-					note: 'G',
-					fret: 3,
-					string: 6
-				},
-				{
-					note: 'A',
-					fret: 0,
-					string: 5
-				},
-				{
-					note: 'B',
-					fret: 2,
-					string: 5
-				},
-				{
-					note: 'C',
-					fret: 3,
-					string: 5
-				},
-				{
-					note: 'D',
-					fret: 0,
-					string: 4
-				},
-				{
-					note: 'E',
-					fret: 2,
-					string: 4
-				},
-				{
-					note: 'G',
-					fret: 0,
-					string: 3
-				},
-				{
-					note: 'A',
-					fret: 2,
-					string: 3
-				},
-				{
-					note: 'B',
-					fret: 0,
-					string: 2
-				},
-				{
-					note: 'C',
-					fret: 1,
-					string: 2
-				},
-				{
-					note: 'D',
-					fret: 3,
-					string: 2
-				},
-				{
-					note: 'E',
-					fret: 0,
-					string: 1
-				},
-				{
-					note: 'F#',
-					fret: 2,
-					string: 1
-				},
-				{
-					note: 'G',
-					fret: 3,
-					string: 1
-				}
-			]
-		}
+		[
+			{
+				id: 'tgVmVCpXMB',
+				name: 'E minor',
+				fret: 0,
+				tab: [
+					{
+						note: 'E',
+						fret: 0,
+						string: 6
+					},
+					{
+						note: 'F#',
+						fret: 2,
+						string: 6
+					},
+					{
+						note: 'G',
+						fret: 3,
+						string: 6
+					},
+					{
+						note: 'A',
+						fret: 0,
+						string: 5
+					},
+					{
+						note: 'B',
+						fret: 2,
+						string: 5
+					},
+					{
+						note: 'C',
+						fret: 3,
+						string: 5
+					},
+					{
+						note: 'D',
+						fret: 0,
+						string: 4
+					},
+					{
+						note: 'E',
+						fret: 2,
+						string: 4
+					},
+					{
+						note: 'G',
+						fret: 0,
+						string: 3
+					},
+					{
+						note: 'A',
+						fret: 2,
+						string: 3
+					},
+					{
+						note: 'B',
+						fret: 0,
+						string: 2
+					},
+					{
+						note: 'C',
+						fret: 1,
+						string: 2
+					},
+					{
+						note: 'D',
+						fret: 3,
+						string: 2
+					},
+					{
+						note: 'E',
+						fret: 0,
+						string: 1
+					},
+					{
+						note: 'F#',
+						fret: 2,
+						string: 1
+					},
+					{
+						note: 'G',
+						fret: 3,
+						string: 1
+					}
+				]
+			}
+		] // E Minor
 	], // E
 	[], // F
 	[], // F#
--- src/pages/guitar/guitar-pages/scales.js
+++ src/pages/guitar/guitar-pages/scales.js
@@ -5,7 +5,7 @@ import GuitarTab from '../../../common/guitar-tab'
 const GuitarScales = props => (
 	<div id="guitar-scales">
 		
-		<GuitarTab scale={props.scalesData[7][0]} />
+		<GuitarTab scale={props.scalesData[7][0][0]} />
 		
 	</div>
 )
