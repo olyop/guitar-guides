bd709d4999d694cdce93a23ab5d4403970e83797
Oliver <oly.rbsc@gmail.com>
2018-05-08 16:40:08 +1000

    Update

--- src/pages/search/search-results.js
+++ src/pages/search/search-results.js
@@ -26,26 +26,29 @@ class PageSearchResults extends React.Component {
 	
 	render() {
 		const matches = this.props.matches
-		return (
-			<div className="search-results-content">
-				<Heading onClick={this.handleContent}
-					active={this.state.content}
-					subtitle={`${matches.length} ${matches.length === 1 ? 'match' : 'matches'}`}>Pages</Heading>
-				{this.state.content ? (
-					<div className="search-results-pages">
-            {matches.map(page => (
-              <Link key={page.id} to={page.path}>
-                <div className="search-results-page">
-                  <i className="material-icons">bookmark</i>
-                  <p>{page.name} - <b>{page.path}</b></p>
-                </div>
-              </Link>
-            ))}
-          </div>
-				) : null}
-        <Ad style={adStyle} />
-			</div>
-		)
+		if (matches.length === 0) { return null }
+		else {
+			return (
+				<div className="search-results-content">
+					<Heading onClick={this.handleContent}
+						active={this.state.content}
+						subtitle={`${matches.length} ${matches.length === 1 ? 'match' : 'matches'}`}>Pages</Heading>
+					{this.state.content ? (
+						<div className="search-results-pages">
+							{matches.map(page => (
+								<Link key={page.id} to={page.path}>
+									<div className="search-results-page">
+										<i className="material-icons">bookmark</i>
+										<p>{page.name} - <b>{page.path}</b></p>
+									</div>
+								</Link>
+							))}
+						</div>
+					) : null}
+					<Ad style={adStyle} />
+				</div>
+			)
+		}
 	}
 }
 
@@ -66,45 +69,48 @@ class ChordSearchResults extends React.Component {
 		this.setState({ content: !this.state.content }) }
 	
 	render() {
-		const matches = this.props.matches 
-		return (
-			<div className="search-results-content">
-				<Heading onClick={this.handleContent}
-					active={this.state.content}
-					subtitle={`${matches.length} ${matches.length === 1 ? 'match' : 'matches'}`}>Chords</Heading>
-				{this.state.content ? (
-					<div>
-            
-						{this.state.more ? (
-							<div className="search-results-chords">
-								{matches.map((chord, index) => (
-									<ChordChart key={chord.id} chord={chord}
-										checkFunction={this.props.updateProgressChords}
-                    completed={includes(this.props.appState.account.progress.guitar.chords, chord.id)}
-										progressLoading={this.props.updateProgressChordsLoading} />
-								))}
-							</div>
-						) : (
-							<div className="search-results-chords">
-								{matches.slice(0,5).map((chord, index) => (
-									<ChordChart key={chord.id} chord={chord}
-										checkFunction={this.props.updateProgressChords}
-                    completed={includes(this.props.appState.account.progress.guitar.chords, chord.id)}
-										progressLoading={this.props.updateProgressChordsLoading} />
-								))}
-							</div>
-						)}
-            
-						{matches.length > 5 ? (
-              <FlatButton onClick={this.handleMore}
-                label={this.state.more ? 'Less...' : 'Show all'} />
-            ) : null}
-            
-					</div>
-				) : null}
-        <Ad style={adStyle} />
-			</div>
-		)
+		const matches = this.props.matches
+		if (matches.length === 0) { return null	}
+		else {
+			return (
+				<div className="search-results-content">
+					<Heading onClick={this.handleContent}
+						active={this.state.content}
+						subtitle={`${matches.length} ${matches.length === 1 ? 'match' : 'matches'}`}>Chords</Heading>
+					{this.state.content ? (
+						<div>
+
+							{this.state.more ? (
+								<div className="search-results-chords">
+									{matches.map((chord, index) => (
+										<ChordChart key={chord.id} chord={chord}
+											checkFunction={this.props.updateProgressChords}
+											completed={includes(this.props.appState.account.progress.guitar.chords, chord.id)}
+											progressLoading={this.props.updateProgressChordsLoading} />
+									))}
+								</div>
+							) : (
+								<div className="search-results-chords">
+									{matches.slice(0,5).map((chord, index) => (
+										<ChordChart key={chord.id} chord={chord}
+											checkFunction={this.props.updateProgressChords}
+											completed={includes(this.props.appState.account.progress.guitar.chords, chord.id)}
+											progressLoading={this.props.updateProgressChordsLoading} />
+									))}
+								</div>
+							)}
+
+							{matches.length > 5 ? (
+								<FlatButton onClick={this.handleMore}
+									label={this.state.more ? 'Less...' : 'Show all'} />
+							) : null}
+
+						</div>
+					) : null}
+					<Ad style={adStyle} />
+				</div>
+			)
+		}
 	}
 }
 
@@ -126,40 +132,40 @@ class RiffSearchResults extends React.Component {
   
   render() {
 		const matches = this.props.matches
-    return (
-      <div className="search-results-content">
-        <Heading onClick={this.handleContent}
-					active={this.state.content}
-					subtitle={`${matches.length} ${matches.length === 1 ? 'match' : 'matches'}`}>Riffs</Heading>
-				{this.state.content ? (
-					<div>
-          
-            {this.state.more ? (
-              <div className="search-results-riffs">
-                {matches.map(riff => (
-                  <Riff key={riff.id}
-                    globalText={this.props.globalText} riff={riff} />
-                ))}
-              </div>
-            ) : (
-              <div className="search-results-riffs">
-                {matches.slice(0,5).map(riff => (
-                  <Riff key={riff.id}
-                    globalText={this.props.globalText} riff={riff} />
-                ))}
-              </div>
-            )}
-            
-            {matches.length > 5 ? (
-              <FlatButton onClick={this.handleMore}
-                label={this.state.more ? 'Less...' : 'Show all'} />
-            ) : null}
-          
-          </div>
-				) : null}
-        <Ad style={adStyle} />
-      </div>
-    )
+		if (matches.length === 0) { return null }
+		else {
+			return (
+				<div className="search-results-content">
+					<Heading onClick={this.handleContent}
+						active={this.state.content}
+						subtitle={`${matches.length} ${matches.length === 1 ? 'match' : 'matches'}`}>Riffs</Heading>
+					{this.state.content ? (
+						<div>
+							{this.state.more ? (
+								<div className="search-results-riffs">
+									{matches.map(riff => (
+										<Riff key={riff.id}
+											globalText={this.props.globalText} riff={riff} />
+									))}
+								</div>
+							) : (
+								<div className="search-results-riffs">
+									{matches.slice(0,5).map(riff => (
+										<Riff key={riff.id}
+											globalText={this.props.globalText} riff={riff} />
+									))}
+								</div>
+							)}
+							{matches.length > 5 ? (
+								<FlatButton onClick={this.handleMore}
+									label={this.state.more ? 'Less...' : 'Show all'} />
+							) : null}
+						</div>
+					) : null}
+					<Ad style={adStyle} />
+				</div>
+			)
+		}
   }
 }
 
@@ -169,33 +175,24 @@ const SearchResults = props => {
   } else if (props.input === '') {
     return null
   } else {
-		const pageMatches = orderBy(findPageMatches(props.globalText.pageStructure, props.input), ['name'], ['asc'])
-    const chordMatches = orderBy(findChordMatches(props.database.chordChooser, props.input), ['fret'], ['asc'])
-    const riffmatches = orderBy(findRiffMatches(props.database.riffs, props.input), ['title'], ['asc'])
-    if (chordMatches.length === 0 && pageMatches.length === 0 && riffmatches.length === 0) {
-      return <SadFace>No Search Results.</SadFace>
-    } else {
+		let unsortedMatches = [
+			{ type: 'pages', matches: orderBy(findPageMatches(props.globalText.pageStructure, props.input), ['name'], ['asc']) },
+			{ type: 'chords', matches: orderBy(findChordMatches(props.database.chordChooser, props.input), ['fret'], ['asc']) },
+			{ type: 'riffs', matches: orderBy(findRiffMatches(props.database.riffs, props.input), ['title'], ['asc']) }
+		]
+		let matches = orderBy(unsortedMatches, ['matches'], ['asc'])
+		console.log()
+    if (matches[0].matches.length === 0 && matches[1].matches.length === 0 && matches[2].matches.length === 0) {
+			return <SadFace>No Search Results.</SadFace>
+		} else {
       return (
         <div className="search-results">
-          
-          {pageMatches.length === 0 ? null : (
-            <PageSearchResults appState={props.appState}
-              matches={pageMatches} />
-          )}
-					
-					{chordMatches.length === 0 ? null : (
-            <ChordSearchResults appState={props.appState}
-              updateProgressChords={props.updateProgressChords}
-							updateProgressChordsLoading={props.updateProgressChordsLoading}
-							matches={chordMatches} />
-          )}
-          
-          {riffmatches.length === 0 ? null : (
-            <RiffSearchResults appState={props.appState}
-              globalText={props.globalText}
-              matches={riffmatches} />
-          )}
-          
+					{matches.map((match, index) => {
+						if (match.type === 'pages') { return <PageSearchResults key={index} appState={props.appState} globalText={props.globalText} matches={match.matches} /> }
+						else if (match.type === 'chords') { return <ChordSearchResults key={index} appState={props.appState} globalText={props.globalText} matches={match.matches} /> }
+						else if (match.type === 'riffs') { return <RiffSearchResults key={index} appState={props.appState} globalText={props.globalText} matches={match.matches} /> }
+						else { return null }
+					})}
         </div>
       )
     }
