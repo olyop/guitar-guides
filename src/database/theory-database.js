ad27542fe20e468baecc05089db3d4eb3883068a
Oliver Plummer <oly.rbsc@gmail.com>
2018-05-24 10:04:35 +1000

    Update theory-database.js

--- src/database/theory-database.js
+++ src/database/theory-database.js
@@ -1,7 +1,21 @@
 const theoryData = {
   notes: ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'],
   chordTypes: ['Major','Minor','Maj7','Min7','Dom7'],
-	scaleTypes: ['Minor','Pentatonic']
+  scaleTypes: ['Minor','Pentatonic'],
+  scales: [
+    ['A','B','C#','D','E','F#','G#'],
+    ['A#','C','D','D#','F','G','A'],
+    ['B','C#','D#','E','F#','G#','A#'],
+    ['C','D','E','F','G','A','B'],
+    ['C#','D#','F','F#','G#','A#','C'],
+    ['D','E','F#','G','A','B','C#'],
+    ['D#','B','C#','D','E','F#','G#'],
+    ['E','B','C#','D','E','F#','G#'],
+    ['F','B','C#','D','E','F#','G#'],
+    ['F#','B','C#','D','E','F#','G#'],
+    ['G','B','C#','D','E','F#','G#'],
+    ['G#','B','C#','D','E','F#','G#']
+  ]
 }
 
 export default theoryData
